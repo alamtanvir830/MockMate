6a4228e8f6b701ddff9d7145f637679473f60bbc
Tanvir Alam <tanviralam@Mac-1964.lan>
2026-08-10 14:51:18 -0400

    feat: add Form 5 C/P (59q) content

---
+++ lib/premade-exams/mcat/form-5/chem-phys.ts
@@ -0,0 +1,17 @@
+import type { MCATSection } from '../types'
+import { cpPassages5_01to05 } from './chem-phys-p01-p05'
+import { cpPassages5_06to10 } from './chem-phys-p06-p10'
+import { cpDiscretes5 } from './chem-phys-discretes'
+
+export const chemPhysSection5: MCATSection = {
+  id: 'chem-phys',
+  title: 'Chemical and Physical Foundations of Biological Systems',
+  abbreviation: 'Chem/Phys',
+  questionCount: 59,
+  seededCount: 59,
+  timeMinutes: 95,
+  breakAfterMinutes: 10,
+  breakType: 'optional',
+  passages: [...cpPassages5_01to05, ...cpPassages5_06to10],
+  discreteQuestions: cpDiscretes5,
+}
