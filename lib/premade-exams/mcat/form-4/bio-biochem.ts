b0ce67d792c615b65690d1b49ab0adb29813c2c7
Tanvir Alam <tanviralam@Mac-1933.lan>
2026-08-10 13:36:59 -0400

    feat: add Form 4 B/B (59q) content

---
+++ lib/premade-exams/mcat/form-4/bio-biochem.ts
@@ -0,0 +1,17 @@
+import type { MCATSection } from '../types'
+import { bbPassages4_01to05 } from './bio-biochem-p01-p05'
+import { bbPassages4_06to10 } from './bio-biochem-p06-p10'
+import { bbDiscretes4 } from './bio-biochem-discretes'
+
+export const bioBiochemSection4: MCATSection = {
+  id: 'bio-biochem',
+  title: 'Biological and Biochemical Foundations of Living Systems',
+  abbreviation: 'Bio/Biochem',
+  questionCount: 59,
+  seededCount: 59,
+  timeMinutes: 95,
+  breakAfterMinutes: 10,
+  breakType: 'optional',
+  passages: [...bbPassages4_01to05, ...bbPassages4_06to10],
+  discreteQuestions: bbDiscretes4,
+}
